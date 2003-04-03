6bf7c50f31569844c15eab380740af35833e032c
Dave Rolsky <autarch@urth.org>
2003-04-03 21:57:19 +0000

    Import of DROLSKY/Exception-Class-1.12 from CPAN.
    gitpan-cpan-distribution: Exception-Class
    gitpan-cpan-version:      1.12
    gitpan-cpan-path:         DROLSKY/Exception-Class-1.12.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

--- Class.pm
+++ Class.pm
@@ -7,7 +7,7 @@ use vars qw($VERSION $BASE_EXC_CLASS %CLASSES);
 
 BEGIN { $BASE_EXC_CLASS ||= 'Exception::Class::Base'; }
 
-$VERSION = '1.11';
+$VERSION = '1.12';
 
 sub import
 {
@@ -203,6 +203,8 @@ BEGIN
 }
 
 1;
+
+sub Classes { sort keys %Exception::Class::CLASSES }
 
 sub throw
 {
@@ -490,6 +492,13 @@ Foo and never declare Foo.
 =head1 Exception::Class::Base CLASS METHODS
 
 =over 4
+
+=item * Classes
+
+Returns a list of the classes that have been defined as subclasses of
+Exception::Class.  Note that this is I<all> the subclasses that have
+been created, so it may include subclasses created by things like CPAN
+modules, etc.
 
 =item * Trace($boolean)
 
