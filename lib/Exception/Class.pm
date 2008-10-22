3ed60afa2a4f11c9ec8f8a4dbd26cc3cc04f9884
Dave Rolsky <autarch@urth.org>
2008-10-22 03:12:00 +0000

    Import of DROLSKY/Exception-Class-1.25 from CPAN.
    gitpan-cpan-distribution: Exception-Class
    gitpan-cpan-version:      1.25
    gitpan-cpan-path:         DROLSKY/Exception-Class-1.25.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

--- lib/Exception/Class.pm
+++ lib/Exception/Class.pm
@@ -10,7 +10,7 @@ use Scalar::Util qw(blessed);
 
 BEGIN { $BASE_EXC_CLASS ||= 'Exception::Class::Base'; }
 
-$VERSION = '1.24';
+$VERSION = '1.25';
 
 sub import
 {
@@ -223,14 +223,37 @@ $VERSION = '1.2';
 # Create accessor routines
 BEGIN
 {
-    my @fields = qw( message pid uid euid gid egid time trace package file line );
+    my @fields = qw( message pid uid euid gid egid time trace );
 
-    no strict 'refs';
     foreach my $f (@fields)
     {
-        *{$f} = sub { my $s = shift; return $s->{$f}; };
+        my $sub = sub { my $s = shift; return $s->{$f}; };;
+
+        no strict 'refs';
+        *{$f} = $sub;
     }
-    *{'error'} = \&message;
+    *error = \&message;
+
+    my %trace_fields =
+        ( package => 'package',
+          file    => 'filename',
+          line    => 'line',
+        );
+
+    while ( my ( $f, $m ) = each %trace_fields )
+    {
+        my $sub = sub
+        {
+            my $s = shift;
+            return $s->{$f} if exists $s->{$f};
+
+            my $frame = $s->trace->frame(0);
+
+            return $s->{$f} = $frame ? $frame->$m() : undef;
+        };
+        no strict 'refs';
+        *{$f} = $sub;
+    }
 }
 
 1;
@@ -304,13 +327,6 @@ sub _initialize
                                 respect_overload => $self->RespectOverload,
                                 max_arg_length   => $self->MaxArgLength,
                               );
-
-    if ( my $frame = $self->trace->frame(0) )
-    {
-        $self->{package} = $frame->package;
-        $self->{line} = $frame->line;
-        $self->{file} = $frame->filename;
-    }
 
     my %fields = map { $_ => 1 } $self->Fields;
     while ( my ($key, $value) = each %p )
@@ -893,6 +909,27 @@ It's a hack but apparently it works.
 Please submit bugs to the CPAN RT system at
 http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Exception%3A%3AClass or
 via email at bug-exception-class@rt.cpan.org.
+
+=head1 DONATIONS
+
+If you'd like to thank me for the work I've done on this module,
+please consider making a "donation" to me via PayPal. I spend a lot of
+free time creating free software, and would appreciate any support
+you'd care to offer.
+
+Please note that B<I am not suggesting that you must do this> in order
+for me to continue working on this particular software. I will
+continue to do so, inasmuch as I have in the past, for as long as it
+interests me.
+
+Similarly, a donation made in this way will probably not make me work
+on this software much more, unless I get so many donations that I can
+consider working on free software full time, which seems unlikely at
+best.
+
+To donate, log into PayPal and send money to autarch@urth.org or use
+the button on this page:
+L<http://www.urth.org/~autarch/fs-donation.html>
 
 =head1 AUTHOR
 
